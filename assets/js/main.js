9efe6e1b522ae98e2969e5006f991c3f58576a86
Ronaldo Maciel <ronaldomacielcamposjunior@gmail.com>
2022-02-28 14:52:56 -0300

    add comments in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,16 @@
-
-
+// FUNÇÃO QUANDO CLICAR NO BOTÃO DO MENU
 function changeInfo(id){
-    document.getElementById('img').src = 'assets/starter-code/assets/destination/image-'+id+'.png'
+    document.getElementById('img').src = 'assets/starter-code/assets/destination/image-'+id+'.png' //ALTERA O SRC DA IMAGEM, PARA O SRC COM O ID PASSADO POR PARAMETRO
 
-    let menu = document.querySelector('.menu-destination').children
-    for(let item of menu){
-        item.classList.remove('select')
+    let menu = document.querySelector('.menu-destination').children //SALVA OS FILHOS DA CLASSE .MENU DESTINATION EM MENU
+    for(let item of menu){ //FOR OF EM MENU
+        item.classList.remove('select') //VAI PERCORRER O ARRAY MENU E VAI REMOVER A CLASSE SELECT
     }
-    document.getElementById(id).classList.add('select')
+    document.getElementById(id).classList.add('select') // ADICIONA A CLASSE SELECT NO ELEMENTO COM O ID INFORMADO NO PARAMETRO
 
-    let info = document.querySelector('.info').children
-    for(let item of info){
-        item.classList.add('d-none')
+    let info = document.querySelector('.info').children //SALVA OS FILHOS DA CLASSE .INFO EM INFO
+    for(let item of info){ //FOR OF EM INFO
+        item.classList.add('d-none') //VAI PERCORRER O ARRAY INFO E VAI ADICIONAR D-NONE A TODOS ELEMENTOS
     }
-    document.querySelector('.info-'+id).classList.remove('d-none')
+    document.querySelector('.info-'+id).classList.remove('d-none') //REMOVE D-NONE DO ELEMENTO COM A CLASSE .INFO- E O ID PASSADO POR PARAMETRO
 }
